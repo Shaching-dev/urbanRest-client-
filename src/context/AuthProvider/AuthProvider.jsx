c222c0f95384116f5c0ec59f149c2e2c194b2f24
unknown <shaching.contact@gmail.com>
2026-04-21 20:17:04 +0600

    firebase authentication and authcontext setup

--- src/context/AuthProvider/AuthProvider.jsx
+++ src/context/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,95 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { AuthContext } from "../AuthContext/AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../../Firebase/firebase.config";
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const registerUserWithEmail = async (email, password) => {
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const signInWithEmail = async (email, password) => {
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const userSignOut = async () => {
+    try {
+      return await signOut(auth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateUserProfile = async (profile) => {
+    try {
+      if (!auth.currentUser) return;
+      return await updateProfile(auth.currentUser, profile);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      const res = await signInWithPopup(auth, googleProvider);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //   user observer
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const userInfo = useMemo(
+    () => ({
+      registerUserWithEmail,
+      signInWithEmail,
+      signInWithGoogle,
+      updateUserProfile,
+      userSignOut,
+      user,
+      loading,
+    }),
+    [user, loading],
+  );
+
+  return (
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
